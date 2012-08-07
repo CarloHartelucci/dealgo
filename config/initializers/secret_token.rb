ab28cf401ce9673f92dd2a10d34885fe40ce4cec
carlohartelucci <charliehartel@yahoo.com>
2012-08-07 11:01:53 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DealGo::Application.config.secret_token = 'c001b97d65fd6d83b67ff5624cf3f3e429e1ad1895722d8dfc1d62c67230af85e3893c6cbfcd6c32916622bc238a9139820b36cb57c2509c952be1aca1169d73'
